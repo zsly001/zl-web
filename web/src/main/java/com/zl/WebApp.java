f715259019e5fbe8966b181e61fe965b7911a146
liuli17 <liuli17@staff.weibo.com>
2023-08-08 20:03:06 +0800

    111

--- web/src/main/java/com/zl/WebApp.java
+++ web/src/main/java/com/zl/WebApp.java
@@ -9,6 +9,10 @@ public class WebApp {
 
     static {
         System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true");
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
+        System.setProperty("com.sun.jndi.cosnaming.object.trustURLCodebase","true");
+        //System.setProperty("java.rmi.server.useCodebaseOnly","false");
+        //System.setProperty("java.rmi.server.hostname","10.222.100.241");
     }
 
     public static void main(String[] args) {
